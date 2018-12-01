4a258625cfd60aa2d00cd6adf9790d87274a7ba0
Li Hao <yfzm1223@sjtu.edu.cn>
2018-12-02 01:42:25 +0800

    init

---
+++ src/main/java/cn/edu/sjtu/ops/grpcdemo/DemoClient.java
@@ -0,0 +1,117 @@
+package cn.edu.sjtu.ops.grpcdemo;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import cn.edu.sjtu.ops.grpcdemo.DemoServiceGrpc.*;
+import io.grpc.stub.StreamObserver;
+
+import java.io.*;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static cn.edu.sjtu.ops.grpcdemo.DemoServiceGrpc.newBlockingStub;
+import static cn.edu.sjtu.ops.grpcdemo.DemoServiceGrpc.newStub;
+
+public class DemoClient {
+
+    private final ManagedChannel channel;
+    private final DemoServiceBlockingStub blockingStub;
+    private final DemoServiceStub asyncStub;
+
+    public DemoClient(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    }
+
+    public DemoClient(ManagedChannelBuilder<?> channelBuilder) {
+        channel = channelBuilder.build();
+        blockingStub = newBlockingStub(channel);
+        asyncStub = newStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void uploadFile(String filename, int chunkSize) throws InterruptedException {
+        final CountDownLatch finishLatch = new CountDownLatch(1);
+        StreamObserver<UploadStatus> responseObserver = new StreamObserver<UploadStatus>() {
+            public void onNext(UploadStatus uploadStatus) {
+                System.out.println("uploadFile: status: "+ String.valueOf(uploadStatus.getCode().getNumber()));
+            }
+
+            public void onError(Throwable throwable) {
+                System.out.println("uploadFile Error!");
+            }
+
+            public void onCompleted() {
+                System.out.println("uploadFile Completed!");
+                finishLatch.countDown();
+            }
+        };
+
+        StreamObserver<Chunk> requestObserver = asyncStub.upload(responseObserver);
+        try {
+            FileInputStream is = new FileInputStream(new File("src/main/resources/" + filename));
+            ByteArrayOutputStream bos = new ByteArrayOutputStream(chunkSize);
+            byte[] b = new byte[chunkSize];
+            int n;
+            while ((n = is.read(b)) != -1) {
+                if (n == chunkSize) {
+                    requestObserver.onNext(Chunk.newBuilder().setContent(ByteString.copyFrom(b)).build());
+                } else {
+                    System.out.println("lalala");
+                    bos.write(b, 0, n);
+                    requestObserver.onNext(Chunk.newBuilder().setContent(ByteString.copyFrom(bos.toByteArray())).build());
+                }
+                if (finishLatch.getCount() == 0) {
+                    // RPC completed or errored before we finished sending.
+                    // Sending further requests won't error, but they will just be thrown away.
+                    return;
+                }
+            }
+            is.close();
+            bos.close();
+        } catch (RuntimeException e) {
+            // Cancel RPC
+            requestObserver.onError(e);
+            throw e;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Mark the end of requests
+        requestObserver.onCompleted();
+
+        // Receiving happens asynchronously
+        if (!finishLatch.await(5, TimeUnit.MINUTES)) {
+            System.out.println("operation can not finish within 5 minutes");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        DemoClient client = new DemoClient("localhost", 8980);
+//        client.uploadFile("testfile.txt", 10);
+        Date start = new Date();
+        client.uploadFile("file-100M", 4 * 1024 * 1024);
+        Date end = new Date();
+        float diff = end.getTime() - start.getTime();
+        System.out.println(String.valueOf(diff / 1000) + "s");
+        client.shutdown();
+    }
+}
+
+
+/*
+64KB : 8.617s
+128KB: 4.523s
+256KB: 2.778s
+512KB: 2.041s
+1MB  : 1.609s
+2MB  : 1.344s
+3MB  : 1.536s
+4000KB: 1.207s
+4MB  : RESOURCE_EXHAUSTED: gRPC message exceeds maximum size 4194304: 4194309
+ */
